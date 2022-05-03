5253aaa02f8b2daf4c2b54cca1af80b44b1ac4ed
unknown <matt.mcquain@gmail.com>
2022-05-02 20:28:34 -0700

    wording changes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,7 @@ import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>2022 Ecommerce All Rights Reserved</p>
+      <p>2022 Petrolnaut | All Rights Reserved</p>
       <p className="icons">
         <AiFillInstagram />
         <AiOutlineTwitter />
